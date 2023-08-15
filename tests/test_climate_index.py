9ecd4f97bdcfe69471aca2b6b5e45c6ebc6fb0f7
Shiv Shankar Singh <ms18006@iisermohali.ac.in>
2023-08-15 16:51:49 +0530

    updating path

---
+++ tests/test_climate_index.py
@@ -0,0 +1,32 @@
+import os
+import xarray as xr
+from climate_library import ClimateIndex
+
+
+
+
+def test_pre_process():
+    # Path to the test data file
+    datafile = os.path.join(os.path.dirname(__file__), 'data', 'cir.nc')
+    
+    # Create an instance of the ClimateIndex class
+    ci = ClimateIndex(datafile)
+
+    # Test the pre_process method
+    ci.pre_process(resample_freq='M')
+    assert ci.data is not None, "Data loading failed."
+    assert 'time' in ci.data.dims, "Time dimension not found in the dataset."
+    assert len(ci.data['time']) == 132, "Time selection or resampling failed."
+
+def test_selected_temperature_indices():
+    # Path to the test data file
+    datafile = os.path.join(os.path.dirname(__file__), 'data', 'cir.nc')
+    
+    # Create an instance of the ClimateIndex class
+    ci = ClimateIndex(datafile)
+    ci.pre_process()
+
+    # Test the selected temperature indices
+    assert isinstance(ci.calculate_tx10p(), xr.DataArray), "Failed to calculate tx10p."
+    assert isinstance(ci.calculate_frost_days(), xr.DataArray), "Failed to calculate frost days."
+    assert isinstance(ci.calculate_warm_spell(), xr.DataArray), "Failed to calculate warm spell."
